0de0f95eb70f1dd6769aa06252b59b099c0e2e0c
Will Crichton <wcrichto@cs.stanford.edu>
2024-05-22 12:23:44 -0700

    Improve hyphenation, fix safari details display

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export let Layout: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <div id="root">{children}</div>
-);
+export let Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  useEffect(() => {
+    let isFirefox = navigator.userAgent.toLowerCase().includes("firefox");
+    let isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+    if (isFirefox) document.documentElement.classList.add("firefox");
+    if (isSafari) document.documentElement.classList.add("safari");
+  }, []);
+  return <div id="root">{children}</div>;
+};
